759755b1141733b98643248e5dce6b2d070dbe06
DeAdrien Hill <deadrienhill@yahoo.com>
2022-12-07 14:42:30 -0600

    completed project 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Car carOne = new Car();
+            {
+                carOne.Make = "Ram";
+                carOne.Model = "Laramie";
+                carOne.Year = 2022;
+
+                Console.WriteLine($"My favorite car is a {carOne.Make} {carOne.Model} {carOne.Year}!");
+                
+            }
+
+            
+
+
+
+
+
+
+
         }
+
+
+
+
+
     }
 }
